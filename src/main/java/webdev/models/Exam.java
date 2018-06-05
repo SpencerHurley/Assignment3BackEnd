1a16ad08823cb94f3573396487edc7ffd6955419
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-06-05 04:19:19 -0400

    final commit

--- src/main/java/webdev/models/Exam.java
+++ src/main/java/webdev/models/Exam.java
@@ -2,7 +2,22 @@ package webdev.models;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
 public class Exam {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="Exam")
 	private List<BaseExamQuestion> questions;
 
 	public List<BaseExamQuestion> getQuestions() {
